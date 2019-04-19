2d6f1b03b5f7a5a83288a18572ddc16343fa1dae
Matthew CntKillMe <willyoumeryme@gmail.com>
2019-04-19 04:11:44 -0700

    constant fix

--- Common/Constants.hpp
+++ Common/Constants.hpp
@@ -5,7 +5,7 @@ namespace Network::Constants
 	/** The default address the server will be hosted on.
 	 * The default address the client will use to connect.
 	 */
-	constexpr const char* ADDRESS = "localhost";
+	constexpr const char* ADDRESS = "127.0.0.1";
 
 	/** The default port the server will be listening to.
 	 * The default port the client will use to connect.
